f28d044745de0c7f8d44ce28461b2743ac7d072d
fcosants <103595090+fcosants@users.noreply.github.com>
2022-06-02 15:45:23 -0300

    Correção na organização

--- test/specs/atividadeAppium.js
+++ test/specs/atividadeAppium.js
@@ -4,65 +4,65 @@ describe("Primeiro teste.apk", () => {
     });
     
     it("Executando teste no App/Alert Dialogs/OK CANCEL DIALOG WITH MESSAGE", async () => {
-    await $('android=new UiSelector().text("App").className("android.widget.TextView")').click();
-    await $('android=new UiSelector().text("Alert Dialogs").className("android.widget.TextView")').click();
-    await $("id=two_buttons").click();
-    await $("id=alertTitle").isDisplayed();
-    await $('android=new UiSelector().text("OK").className("android.widget.Button")').click();
-    });
+        await $('android=new UiSelector().text("App").className("android.widget.TextView")').click();
+        await $('android=new UiSelector().text("Alert Dialogs").className("android.widget.TextView")').click();
+        await $("id=two_buttons").click();
+        await $("id=alertTitle").isDisplayed();
+        await $('android=new UiSelector().text("OK").className("android.widget.Button")').click();
+        });
 
     it("Executando teste em App/Alert Dialogs/LIST DIALOG", async () => {
-    await $('android=new UiSelector().text("App").className("android.widget.TextView")').click();
-    await $('android=new UiSelector().text("Alert Dialogs").className("android.widget.TextView")').click();
-    await $("id=select_button").click();
-    await $('android=new UiSelector().text("Header title").className("android.widget.TextView")').isDisplayed();
-    });
+        await $('android=new UiSelector().text("App").className("android.widget.TextView")').click();
+        await $('android=new UiSelector().text("Alert Dialogs").className("android.widget.TextView")').click();
+        await $("id=select_button").click();
+        await $('android=new UiSelector().text("Header title").className("android.widget.TextView")').isDisplayed();
+        });
 
     it("Executando teste em App/Fragment/Hide and Show", async () => {
-    await $('android=new UiSelector().text("App").className("android.widget.TextView")').click();
-    await $('android=new UiSelector().text("Fragment").className("android.widget.TextView")').click();
-    await $('android=new UiSelector().text("Hide and Show").className("android.widget.TextView")').click();
-    await $('android=new UiSelector().text("Demonstration of hiding and showing fragments.").className("android.widget.TextView")').click();
-    await $("id=frag1hide").click();
-    await $("id=frag2hide").click();
-    const clicarBotao = await $("id=frag1hide").getText();
-    expect(clicarBotao).toBe("SHOW");
-    const clicarBotao1 = await $("id=frag2hide").getText();
-    expect(clicarBotao1).toBe("SHOW");
-    });
+        await $('android=new UiSelector().text("App").className("android.widget.TextView")').click();
+        await $('android=new UiSelector().text("Fragment").className("android.widget.TextView")').click();
+        await $('android=new UiSelector().text("Hide and Show").className("android.widget.TextView")').click();
+        await $('android=new UiSelector().text("Demonstration of hiding and showing fragments.").className("android.widget.TextView")').click();
+        await $("id=frag1hide").click();
+        await $("id=frag2hide").click();
+        const clicarBotao = await $("id=frag1hide").getText();
+        expect(clicarBotao).toBe("SHOW");
+        const clicarBotao1 = await $("id=frag2hide").getText();
+        expect(clicarBotao1).toBe("SHOW");
+        });
 
     it("Executando teste em App/Action Bar/Display options, e a opção DISPLAY_HOME_AS_UP", async () => {
-    await $('android=new UiSelector().text("App").className("android.widget.TextView")').click();
-    await $('android=new UiSelector().text("Action Bar").className("android.widget.TextView")').click();
-    await $('android=new UiSelector().text("Display Options").className("android.widget.TextView")').click();
-    await $("id=toggle_show_home").click();
-    const trechoTitulo = await $("android.widget.TextView").getText();
-    expect(trechoTitulo).toBe("App/Action Bar/Display Options");
-    });
+        await $('android=new UiSelector().text("App").className("android.widget.TextView")').click();
+        await $('android=new UiSelector().text("Action Bar").className("android.widget.TextView")').click();
+        await $('android=new UiSelector().text("Display Options").className("android.widget.TextView")').click();
+        await $("id=toggle_show_home").click();
+        const trechoTitulo = await $("android.widget.TextView").getText();
+        expect(trechoTitulo).toBe("App/Action Bar/Display Options");
+        });
 
     it("Executando teste em App/Action Bar/Display options, e a opção DISPLAY_SHOW_TITLE", async () => {
-    await $('android=new UiSelector().text("App").className("android.widget.TextView")').click();
-    await $('android=new UiSelector().text("Action Bar").className("android.widget.TextView")').click();
-    await $('android=new UiSelector().text("Display Options").className("android.widget.TextView")').click();
-    await $("id=toggle_show_title").click();
-    const trechoTitulo = await $("android.view.ViewGroup").getText();
-    expect(trechoTitulo).toBe("");
-    });
+        await $('android=new UiSelector().text("App").className("android.widget.TextView")').click();
+        await $('android=new UiSelector().text("Action Bar").className("android.widget.TextView")').click();
+        await $('android=new UiSelector().text("Display Options").className("android.widget.TextView")').click();
+        await $("id=toggle_show_title").click();
+        const trechoTitulo = await $("android.view.ViewGroup").getText();
+        expect(trechoTitulo).toBe("");
+        });
 
     it("Executando teste em App/Action Bar/Display options, e a opção DISPLAY_SHOW_CUSTOM", async () => {
-    await $('android=new UiSelector().text("App").className("android.widget.TextView")').click();
-    await $('android=new UiSelector().text("Action Bar").className("android.widget.TextView")').click();
-    await $('android=new UiSelector().text("Display Options").className("android.widget.TextView")').click();
-    await $("id=toggle_show_custom").click();
-    const botaoSuperior = await $("android.widget.Button").getText();
-    expect(botaoSuperior).toBe("CUSTOM VIEW!");
-    });
+        await $('android=new UiSelector().text("App").className("android.widget.TextView")').click();
+        await $('android=new UiSelector().text("Action Bar").className("android.widget.TextView")').click();
+        await $('android=new UiSelector().text("Display Options").className("android.widget.TextView")').click();
+        await $("id=toggle_show_custom").click();
+        const botaoSuperior = await $("android.widget.Button").getText();
+        expect(botaoSuperior).toBe("CUSTOM VIEW!");
+        });
 
     it("Executando teste em App/Action Bar/Display options, e a opção DISPLAY_SHOW_NAVIGATION", async () => {
-    await $('android=new UiSelector().text("App").className("android.widget.TextView")').click();
-    await $('android=new UiSelector().text("Action Bar").className("android.widget.TextView")').click();
-    await $('android=new UiSelector().text("Display Options").className("android.widget.TextView")').click();
-    await $("id=toggle_navigation").click();
-    });
+        await $('android=new UiSelector().text("App").className("android.widget.TextView")').click();
+        await $('android=new UiSelector().text("Action Bar").className("android.widget.TextView")').click();
+        await $('android=new UiSelector().text("Display Options").className("android.widget.TextView")').click();
+        await $("id=toggle_navigation").click();
+        });
 
 });
